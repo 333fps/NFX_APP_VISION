7757276027fe2a78ac77461b443ad90120bbcd2e
333fps <ronan.fesselier@gmail.com>
2025-01-26 17:07:54 +0100

    Cleanup

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -11,6 +11,9 @@ add_dependencies( ${PROJECT_NAME} "copy_resources_folder_${PROJECT_NAME}" )
 get_target_property(nfxLIBS_COMPILE_DEFINITIONS nfxCore INTERFACE_COMPILE_DEFINITIONS)
 message(STATUS "nfxLibs definitions: ${nfxLIBS_COMPILE_DEFINITIONS}")
 
+  find_package(imgui CONFIG REQUIRED)
+  target_link_libraries(${PROJECT_NAME} PRIVATE imgui::imgui)
+
 target_link_libraries( ${PROJECT_NAME} PUBLIC
 											nfxApplication
 											nfxGraphicsCore
