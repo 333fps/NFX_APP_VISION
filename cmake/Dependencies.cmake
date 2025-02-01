75073dbf0a3c8ac022d67ee2ef2803df97bb7443
333fps <ronan.fesselier@gmail.com>
2025-02-01 18:28:06 +0100

    Cleanup

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -11,15 +11,6 @@ add_dependencies( ${PROJECT_NAME} "copy_resources_folder_${PROJECT_NAME}" )
 get_target_property(nfxLIBS_COMPILE_DEFINITIONS nfxCore INTERFACE_COMPILE_DEFINITIONS)
 message(STATUS "nfxLibs definitions: ${nfxLIBS_COMPILE_DEFINITIONS}")
 
-  find_package(imgui CONFIG REQUIRED)
-  target_link_libraries(${PROJECT_NAME} PRIVATE imgui::imgui)
-
-	  find_package(magic_enum CONFIG REQUIRED)
-	target_link_libraries(${PROJECT_NAME} PRIVATE magic_enum::magic_enum)
-
-		find_package(GLEW REQUIRED)
-	target_link_libraries(${PROJECT_NAME} PRIVATE GLEW::GLEW)
-
 target_link_libraries( ${PROJECT_NAME} PUBLIC
 											nfxApplication
 											nfxGraphicsCore
