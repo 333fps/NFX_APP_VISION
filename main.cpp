e83606f85fcae51d17f1d63429ef44eb3e3839b2
333fps <ronan.fesselier@gmail.com>
2025-03-09 14:13:58 +0100

    Cleanup

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include "App/App.h"
 
+#include <spdlog/spdlog.h>
+
 int main(int argc, char* argv[])
 {
+	spdlog::set_level(spdlog::level::debug);
+
 	App app{ "NFX_APP_VISION", "1.0", 1920, 1080 };
 
 	return app.run(argc, argv);
